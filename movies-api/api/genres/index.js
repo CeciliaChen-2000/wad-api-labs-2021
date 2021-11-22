24b9ff82a49c89d15956617e03be7d8ef3e30d8b
Liangyu Chen <20095989@mail.wit.ie>
2021-11-22 14:47:27 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import uniqid from 'uniqid'
-import { genres } from './genresData';
+import Genre from './genreModel';
+import asyncHandler from 'express-async-handler';
 
-const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
+const router = express.Router(); // eslint-disable-line
+
+// Get all genres
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
 export default router;
